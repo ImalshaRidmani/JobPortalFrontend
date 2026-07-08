82fd713aa5956cbb23076734bb42f146d4e68712
Imalsha <Imalsha@msl.com>
2026-07-08 16:29:13 +0530

    Update job service to add new api

--- src/services/jobService.ts
+++ src/services/jobService.ts
@@ -5,3 +5,8 @@ export const getJobs = async (): Promise<Job[]> => {
   const response = await api.get<Job[]>("/jobs");
   return response.data;
 };
+
+export const getJobById = async (id: string): Promise<Job> => {
+  const res = await api.get(`/jobs/${id}`);
+  return res.data;
+};
